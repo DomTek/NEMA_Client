4878dac701ab7b1b36fa08fce91dc19dcc24b869
DomTek <dominikjutzi@gmail.com>
2023-05-07 19:09:00 +1000

    Added a GridBagLayout to help with placing items at the correct spot

--- src/main/java/com/mycompany/nema_client/NEMA_Client.java
+++ src/main/java/com/mycompany/nema_client/NEMA_Client.java
@@ -21,51 +21,62 @@ public class NEMA_Client extends JFrame {
     private JList<String> fireList;
     private JButton updateButton, respondButton, exitButton;
 
-    public NEMA_Client() {
+public NEMA_Client() {
 
-        // Set GUI window properties
-        setTitle("NEMA Client");
-        setSize(500, 600);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    // Set GUI window properties
+    setTitle("NEMA Client");
+    setSize(600, 600);
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        // Initialize GUI components
-        updateButton = new JButton("Update Report");
-        reportArea = new JTextArea();
-        severityLabel = new JLabel("Select Fire Severity:");
-        severityComboBox = new JComboBox<>(new String[]{"Low", "Medium", "High"});
-        fireLabel = new JLabel("Select Fire:");
-        fireList = new JList<>(new String[]{"Fire 1", "Fire 2", "Fire 3"});
-        respondButton = new JButton("Respond Truck");
-        exitButton = new JButton("Exit");
-
+    // Initialize GUI components
+    updateButton = new JButton("Update Report");
+    reportArea = new JTextArea();
+    severityLabel = new JLabel("Select Fire Severity:");
+    severityComboBox = new JComboBox<>(new String[]{"Low", "Medium", "High"});
+    fireLabel = new JLabel("Select Fire:");
+    fireList = new JList<>(new String[]{"Fire 1", "Fire 2", "Fire 3"});
+    respondButton = new JButton("Respond Truck");
+    exitButton = new JButton("Exit");
+    
         // Add action listener to exit button
-        exitButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                System.exit(0); // Terminate the Java Virtual Machine
-            }
-        });
+    exitButton.addActionListener(new ActionListener() {
+        public void actionPerformed(ActionEvent e) {
+            System.exit(0); // Terminate the Java Virtual Machine
+        }
+    });
 
-// Add GUI components to content pane
-        Container contentPane = getContentPane();
-        contentPane.setLayout(new BorderLayout());
-        JPanel topPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
-        topPanel.add(updateButton);
-        topPanel.add(exitButton);
-        contentPane.add(topPanel, BorderLayout.NORTH);
-        JScrollPane reportScrollPane = new JScrollPane(reportArea);
-        contentPane.add(reportScrollPane, BorderLayout.CENTER);
-        JPanel bottomPanel = new JPanel(new GridLayout(3, 3, 10, 10)); // Add 10-pixel space between components
-        bottomPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10)); // Add 10-pixel padding around the panel
-        bottomPanel.add(severityLabel);
-        bottomPanel.add(severityComboBox);
-        bottomPanel.add(fireLabel);
-        bottomPanel.add(fireList);
-        respondButton.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10)); // Add 10-pixel empty border
-        bottomPanel.add(respondButton);
-
-        contentPane.add(bottomPanel, BorderLayout.SOUTH);
-    }
-
+    // Add GUI components to content pane
+    Container contentPane = getContentPane();
+    contentPane.setLayout(new BorderLayout());
+    JPanel topPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+    topPanel.add(updateButton);
+    topPanel.add(exitButton);
+    contentPane.add(topPanel, BorderLayout.NORTH);
+    JScrollPane reportScrollPane = new JScrollPane(reportArea);
+    contentPane.add(reportScrollPane, BorderLayout.CENTER);
+    JPanel bottomPanel = new JPanel(new GridBagLayout());
+    GridBagConstraints c = new GridBagConstraints();
+    c.gridx = 0;
+    c.gridy = 0;
+    c.anchor = GridBagConstraints.CENTER;
+    c.fill = GridBagConstraints.HORIZONTAL;
+    c.weightx = 1.0;
+    c.insets = new Insets(0, 10, 0, 10); 
+    bottomPanel.add(severityLabel, c);
+    c.gridx = 1;
+    c.insets = new Insets(0, 0, 0, 10); 
+    bottomPanel.add(severityComboBox, c);
+    c.gridx = 2;
+    c.insets = new Insets(0, 0, 0, 10); 
+    bottomPanel.add(fireLabel, c);
+    c.gridx = 3;
+    c.insets = new Insets(0, 0, 0, 10); 
+    bottomPanel.add(fireList, c);
+    c.gridx = 4;
+    c.insets = new Insets(0, 0, 0, 10);
+    bottomPanel.add(respondButton, c);
+    contentPane.add(bottomPanel, BorderLayout.SOUTH);
+}
     public static void main(String[] args) {
         NEMA_Client client = new NEMA_Client();
         client.setVisible(true);
