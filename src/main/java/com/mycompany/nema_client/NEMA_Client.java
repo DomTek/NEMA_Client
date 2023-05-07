af68022f79524c5f813d8057120f46ba8720150a
DomTek <dominikjutzi@gmail.com>
2023-05-07 18:45:24 +1000

    Changed the layout and added a action listener to the Exit button to close the application

--- src/main/java/com/mycompany/nema_client/NEMA_Client.java
+++ src/main/java/com/mycompany/nema_client/NEMA_Client.java
@@ -1,19 +1,20 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package com.mycompany.nema_client;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  *
  * @author Nighthawk
  */
-public class NEMA_Client extends JFrame{
+public class NEMA_Client extends JFrame {
 
-      // Declare GUI components
+    // Declare GUI components
     private JLabel severityLabel, fireLabel;
     private JTextArea reportArea;
     private JComboBox<String> severityComboBox;
@@ -24,7 +25,7 @@ public class NEMA_Client extends JFrame{
 
         // Set GUI window properties
         setTitle("NEMA Client");
-        setSize(600, 500);
+        setSize(500, 600);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         // Initialize GUI components
@@ -37,21 +38,31 @@ public class NEMA_Client extends JFrame{
         respondButton = new JButton("Respond Truck");
         exitButton = new JButton("Exit");
 
-        // Add GUI components to content pane
+        // Add action listener to exit button
+        exitButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0); // Terminate the Java Virtual Machine
+            }
+        });
+
+// Add GUI components to content pane
         Container contentPane = getContentPane();
         contentPane.setLayout(new BorderLayout());
         JPanel topPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
         topPanel.add(updateButton);
+        topPanel.add(exitButton);
         contentPane.add(topPanel, BorderLayout.NORTH);
         JScrollPane reportScrollPane = new JScrollPane(reportArea);
         contentPane.add(reportScrollPane, BorderLayout.CENTER);
-        JPanel bottomPanel = new JPanel(new GridLayout(2, 2));
+        JPanel bottomPanel = new JPanel(new GridLayout(3, 3, 10, 10)); // Add 10-pixel space between components
+        bottomPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10)); // Add 10-pixel padding around the panel
         bottomPanel.add(severityLabel);
         bottomPanel.add(severityComboBox);
         bottomPanel.add(fireLabel);
         bottomPanel.add(fireList);
+        respondButton.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10)); // Add 10-pixel empty border
         bottomPanel.add(respondButton);
-        bottomPanel.add(exitButton);
+
         contentPane.add(bottomPanel, BorderLayout.SOUTH);
     }
 
